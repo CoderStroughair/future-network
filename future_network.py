37f9ed8a65d282bc3f1e59f36957ad3a19f9ce88
irishMTS <stroughm@tcd.ie>
2015-02-17 08:25:30 +0000

    Finished Python Script

--- future_network.py
+++ future_network.py
@@ -1,62 +1,55 @@
-import RPi.GPIOasGPIO
+import RPi.GPIO as GPIO
 import pifacedigitalio
 import signal
 import json
+from time import sleep
 
 p = pifacedigitalio.PiFaceDigital()
 
 #### Assign pins to the various outputs ####
-windmill_0 = 0
-windmill_1 = 1
-lights_primary = 2
-lights_secondary = 3
-power_stations = 4
+windmill = 0
+lightsPrimary = 1
+chargers = 2
+mode = 0
+delay = 5
 
 def mode_change(signum,frame):
-        mode = mode+1
+        global mode 
+	global delay
+	mode = mode+1
         #### Currently no Requirement for Blackout Mode ####
         if mode > 2:
                 mode = 0
         powermode(mode)
-        signal.alarm(30)
+        signal.alarm(delay)
         
 def powermode(mode):
         #### Full Power Mode - All Lights on ####
         if mode==0:
-                p.leds[windmill_0].turn_on()
-                p.leds[windmill_1].turn_on()
-                p.leds[lights_primary].turn_on()
-                p.leds[lights_secondary].turn_on()
-                p.leds[power_stations].turn_off()
-        #### Half Power Mode - Windmill at Half Mast - All Lights on with Power Stations ####
+                p.leds[windmill].turn_on()
+                p.leds[lightsPrimary].turn_on()
+		p.leds[chargers].turn_on()
+        #### Power Station Mode - Windmill at Rest - All Lights on with Power Stations ####
         elif mode==1:
-                p.leds[windmill_0].turn_on()
-                p.leds[windmill_1].turn_off()
-                p.leds[lights_primary].turn_on()
-                p.leds[lights_secondary].turn_on()
-                p.leds[power_stations].turn_on()
-        #### Low Power Mode - Windmill at Rest - Power Stations, Secondary Lights and Chargers off ####
+                p.leds[windmill].turn_off()
+                p.leds[lightsPrimary].turn_on()
+		p.leds[chargers].turn_on()
+        #### Low Demand Mode - Windmill at Rest - Power Stations, Secondary Lights and Chargers off ####
         elif mode==2:
-                p.leds[windmill_0].turn_on()
-                p.leds[windmill_1].turn_off()
-                p.leds[lights_primary].turn_on()
-                p.leds[lights_secondary].turn_off()
-                p.leds[power_stations].turn_off()
-        #### Blackout Mode - No Power - All Lights off ####
-        elif mode==3:
-                p.leds[windmill_0].turn_off()
-                p.leds[windmill_1].turn_off()
-                p.leds[lights_primary].turn_off()
-                p.leds[lights_secondary].turn_off()
-                p.leds[power_stations].turn_off()
+                p.leds[windmill].turn_off()
+                p.leds[lightsPrimary].turn_off()
+		p.leds[chargers].turn_off()
 
 #### Initialise the Lights to Full Power Mode ####
 powermode(0)
-mode=0
 signal.signal(signal.SIGALRM,mode_change)
-signal.alarm(30)
-while true:
-		obj	= [{"mode": mode}]
-		data = json.dump(obj)
-        with open("state.txt",w) as f:
-                f.writeline(data)
+signal.alarm(delay)
+while True:
+	obj= [{"mode": mode}]
+	data = json.dumps(obj)
+	data2 = json.dumps(mode)
+     	with open("state.txt","w") as f:
+               	f.write(data)
+	with open("state2.txt","w") as f:
+		f.write(data2)
+	sleep(1)
